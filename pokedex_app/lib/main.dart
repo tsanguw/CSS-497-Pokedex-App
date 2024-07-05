582051bc6e901b02a66e86500e02b1735ed90136
Daniel Tsang <dtsang80@uw.edu>
2024-07-04 20:56:00 -0700

    feat: added navbar & drawer

--- pokedex_app/lib/main.dart
+++ pokedex_app/lib/main.dart
@@ -9,11 +9,99 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _selectedTitle = 'Pokedex'; // Initial title
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              color: Colors.white,
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
         ),
+        centerTitle: true,
+        title: Text(_selectedTitle, style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.red,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+              child: Text('Pokedex', style: TextStyle(color: Colors.white)),
+            ),
+            ListTile(
+              title: const Text('Pokemon'),
+              onTap: () {
+                setState(() {
+                  _selectedTitle = 'Pokemon';
+                });
+                Navigator.of(context).pop(); // Close the drawer
+              },
+            ),
+            ListTile(
+              title: const Text('Moves'),
+              onTap: () {
+                setState(() {
+                  _selectedTitle = 'Moves';
+                });
+                Navigator.of(context).pop(); // Close the drawer
+              },
+            ),
+            ListTile(
+              title: const Text('Abilities'),
+              onTap: () {
+                setState(() {
+                  _selectedTitle = 'Abilities';
+                });
+                Navigator.of(context).pop(); // Close the drawer
+              },
+            ),
+            ListTile(
+              title: const Text('Items'),
+              onTap: () {
+                setState(() {
+                  _selectedTitle = 'Items';
+                });
+                Navigator.of(context).pop(); // Close the drawer
+              },
+            ),
+            ListTile(
+              title: const Text('Natures'),
+              onTap: () {
+                setState(() {
+                  _selectedTitle = 'Natures';
+                });
+                Navigator.of(context).pop(); // Close the drawer
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Text('Hello World!'),
       ),
     );
   }
